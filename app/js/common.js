34e4a62fd85a45977d872e6cbcd3484a97c57b2b
Taras-B <taras5475@gmail.com>
2019-02-26 15:06:16 +0200

    add footer and button to top

--- app/js/common.js
+++ app/js/common.js
@@ -10,6 +10,18 @@ $(function() {
      return false;
   });
 
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > $(this).height()) {
+      $('.top-btn').addClass('active');
+    } else {
+      $('.top-btn').removeClass('active')
+    }
+  });
+
+  $('.top-btn').click(function() {
+    $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+  });
+
   $('#hamburger-icon').click(function(){
      $('.mtoggle').toggleClass('active')
    });
